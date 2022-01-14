e014413ef76fe307c977262607340dc92098fbc3
jinhwansuh <nick02190219@gmail.com>
2022-01-14 15:46:08 +0900

    design: Counter 스타일 변경

--- 05-Counter/src/App.js
+++ 05-Counter/src/App.js
@@ -2,29 +2,30 @@ import { Button, Count, Text } from './components/index.js';
 
 export default function App({ mainEl, initialState }) {
   const containerEl = document.createElement('div');
-  const counterEl = document.createElement('div');
   const buttonEl = document.createElement('div');
   containerEl.className = 'container';
-  counterEl.className = 'counter';
+
   this.state = initialState;
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    number.setState(this.state);
+  };
 
   const onIncrease = () => {
     const nextState = this.state + 1;
-    this.state = nextState;
-    number.setState(nextState);
+    this.setState(nextState);
   };
   const onDecrease = () => {
     const nextState = this.state - 1;
-    this.state = nextState;
-    number.setState(nextState);
+    this.setState(nextState);
   };
 
-  new Text({ targetEl: counterEl });
-  const number = new Count({ targetEl: counterEl, initialState });
+  new Text({ targetEl: containerEl });
+  const number = new Count({ targetEl: containerEl, initialState });
   new Button({ targetEl: buttonEl, onChange: onIncrease, state: 'Increase' });
   new Button({ targetEl: buttonEl, onChange: onDecrease, state: 'Decrease' });
 
-  counterEl.appendChild(buttonEl);
-  containerEl.appendChild(counterEl);
+  containerEl.appendChild(buttonEl);
   mainEl.appendChild(containerEl);
 }
