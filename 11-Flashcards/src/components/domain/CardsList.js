87dbbcc54ead98b2d064e84c955f680333669777
jinhwansuh <nick02190219@gmail.com>
2022-01-15 23:03:09 +0900

    feat: 클릭했을 때 answer 나오도록 설정

--- 11-Flashcards/src/components/domain/CardsList.js
+++ 11-Flashcards/src/components/domain/CardsList.js
@@ -2,19 +2,30 @@ export default function CardsList({ targetEl, initialState }) {
   const cardsListEl = document.createElement('div');
   cardsListEl.className = 'card-list';
   this.state = initialState;
+  this.displayState = false;
 
   this.setState = (nextState) => {
     this.state = nextState;
     this.render();
   };
 
+  cardsListEl.addEventListener('click', (e) => {
+    const targetEl = e.target;
+    if (targetEl.className === 'card') {
+      const cardEl = e.target.closest('.card');
+      const answerEl = cardEl.querySelector('.answer');
+      this.displayState = !this.displayState;
+      answerEl.style.display = this.displayState ? 'block' : 'none';
+    }
+  });
+
   this.render = () => {
     cardsListEl.innerHTML = `
       ${this.state
         ?.map(
           (card) => `<div class="card">
-        <div>${card.Question}</div>
-        <div>${card.Answer}</div>
+        <div class="question">${card.Question}</div>
+        <div class="answer" style="display: none">${card.Answer}</div>
       </div>`
         )
         .join('')}
