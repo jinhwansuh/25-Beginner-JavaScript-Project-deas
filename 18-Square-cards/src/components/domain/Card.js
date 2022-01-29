e6ebb962bdc1e294cf6eb54ea06ffb44f3a3fab2
jinhwansuh <nick02190219@gmail.com>
2022-01-29 16:09:33 +0900

    feat: project data 추가

--- 18-Square-cards/src/components/domain/Card.js
+++ 18-Square-cards/src/components/domain/Card.js
@@ -7,13 +7,8 @@ export default function Card({ targetEl, state }) {
 
   const { link, imageUrl, title, description, color } = state;
 
-  // props :
-  // 링크, 이미지, Title, 설명, Style (backgroundColor)
-  // link, imageUrl, title, description, style
-
   cardEl.addEventListener('click', (e) => {
-    console.log('click');
-    // window.location.href = link;
+    window.location.href = link;
   });
 
   new Image({
