c5c442377e6e4fa1158363cafefb7e1c49b175aa
jinhwansuh <nick02190219@gmail.com>
2022-01-29 16:28:50 +0900

    rename: imageArray 파일 분리

---
+++ 06-Image-carousel/src/assets/images.js
@@ -0,0 +1,9 @@
+export const imageArray = [
+  'https://media.istockphoto.com/photos/change-arrow-with-smaller-but-many-obstacles-picture-id1302958092?b=1&k=20&m=1302958092&s=170667a&w=0&h=5gkT1Qon1BMxeb1TrsKy5AOckLBL9dnSCtg5zchhbBg=',
+  'https://media.istockphoto.com/photos/sports-girl-who-wants-to-start-the-new-year-2022-concept-of-new-in-picture-id1344917658?b=1&k=20&m=1344917658&s=170667a&w=0&h=sAmShJ9XG9bpsJOfMfgZVJczyiDXVlJyk19sIuT32RM=',
+  'https://media.istockphoto.com/photos/innovation-standing-out-from-the-crowd-picture-id1291181797?b=1&k=20&m=1291181797&s=170667a&w=0&h=xlzPRR0nTnZUvNcxP3DsQC3P1Gv-qxOQfoQC8s7vB0o=',
+  'https://media.istockphoto.com/photos/copy-space-of-abstract-background-with-butterfly-growth-evolution-picture-id1288604066?b=1&k=20&m=1288604066&s=170667a&w=0&h=1-7DLuzPmQkDco_Ov03tGinLio-Lj1zeSARMTm2EbFs=',
+  'https://media.istockphoto.com/photos/different-sphere-other-side-of-border-picture-id1297426893?b=1&k=20&m=1297426893&s=170667a&w=0&h=GCxvnUyMRq62M4KjvqcgNPwVsYHHq1jvlZ157-IRYf4=',
+  'https://images.unsplash.com/photo-1533073526757-2c8ca1df9f1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hhbmdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+  'https://images.unsplash.com/photo-1477414348463-c0eb7f1359b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2hhbmdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+];
