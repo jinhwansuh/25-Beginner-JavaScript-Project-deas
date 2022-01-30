93c0d3882e95ab4d51ef11e33968b800abbe4bdd
jinhwansuh <nick02190219@gmail.com>
2022-01-30 14:53:57 +0900

    refactor: color값을 명시적으로 받도록 설정

--- 18-Square-cards/src/components/domain/Details.js
+++ 18-Square-cards/src/components/domain/Details.js
@@ -3,7 +3,7 @@ import { Text } from '../base/index.js';
 export default function Details({ targetEl, title, description, color }) {
   const detailsEl = document.createElement('div');
   detailsEl.className = 'details';
-  detailsEl.style = `background-color: ${color}`;
+  detailsEl.style.backgroundColor = color;
 
   new Text({ targetEl: detailsEl, tag: 'h1', text: title });
   new Text({
