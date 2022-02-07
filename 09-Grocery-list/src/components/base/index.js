8073df6dd6c9323d00bd23ea7091e814050fd2ac
jinhwansuh <nick02190219@gmail.com>
2022-02-08 01:16:59 +0900

    design: css 적용

--- 09-Grocery-list/src/components/base/index.js
+++ 09-Grocery-list/src/components/base/index.js
@@ -1,4 +1,3 @@
 export { default as Text } from './Text.js';
-export { default as Button } from './Button.js';
 export { default as Input } from './Input.js';
 export { default as GoogleIcon } from './GoogleIcon.js';
