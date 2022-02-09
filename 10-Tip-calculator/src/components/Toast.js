32ee7a20de20993983351f3062c132318fee7f66
jinhwansuh <nick02190219@gmail.com>
2022-02-09 20:51:57 +0900

    feat: toast message 구현

---
+++ 10-Tip-calculator/src/components/Toast.js
@@ -0,0 +1,16 @@
+export default function Toast({ time }) {
+  const toastEl = document.createElement('div');
+  toastEl.className = 'toast';
+  toastEl.style.display = 'none';
+
+  this.render = (message) => {
+    toastEl.innerHTML = message;
+    toastEl.style.display = 'block';
+
+    setTimeout(() => {
+      toastEl.style.display = 'none';
+    }, time);
+  };
+
+  document.body.appendChild(toastEl);
+}
--- 10-Tip-calculator/src/components/index.js
+++ 10-Tip-calculator/src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Button } from './Button.js';
+export { default as Text } from './Text.js';
 export { default as Input } from './Input.js';
+export { default as Toast } from './Toast.js';
 export { default as SelectBox } from './SelectBox.js';
