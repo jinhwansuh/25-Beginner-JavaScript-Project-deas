099fdf1c1131d5211a21113046607e4882164abe
jinhwansuh <nick02190219@gmail.com>
2022-02-17 14:53:10 +0900

    feat: wikipedia 기본 구조

---
+++ 20-Wikipedia-API/src/api/wikipedia.js
@@ -0,0 +1,5 @@
+/* 
+  API 예시
+  https://en.wikipedia.org/w/api.php?action=query&origin=*&pilicense=any&prop=pageimages&pithumbsize=250&format=json&gsrlimit=500&gsroffset=18&generator=search&gsrsearch=cola
+  https://en.wikipedia.org/?curid=7605
+ */
