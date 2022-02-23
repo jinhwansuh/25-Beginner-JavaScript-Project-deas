bb76b239636f57905b131ef46a03ccadab76e0ee
jinhwansuh <nick02190219@gmail.com>
2022-02-23 14:26:57 +0900

    feat: wikiAPI 연결

--- 20-Wikipedia-API/src/api/wikipedia.js
+++ 20-Wikipedia-API/src/api/wikipedia.js
@@ -3,3 +3,18 @@
   https://en.wikipedia.org/w/api.php?action=query&origin=*&pilicense=any&prop=pageimages&pithumbsize=250&format=json&gsrlimit=500&gsroffset=18&generator=search&gsrsearch=cola
   https://en.wikipedia.org/?curid=7605
  */
+const WIKIPEDIA_END_POINT =
+  'https://en.wikipedia.org/w/api.php?action=query&origin=*&pilicense=any&prop=pageimages&pithumbsize=250&format=json&gsrlimit=500&gsroffset=18&generator=search&gsrsearch=';
+
+export const getWikiData = async (value) => {
+  try {
+    const res = await fetch(`${WIKIPEDIA_END_POINT}${value}`);
+
+    if (!res.ok) {
+      throw new Error('API Call Fail');
+    }
+    return await res.json();
+  } catch (e) {
+    alert(e.message);
+  }
+};
