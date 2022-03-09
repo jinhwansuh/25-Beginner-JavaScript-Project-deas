227afc49ad490bceebfdc0370d7ee5de95bbae2f
jinhwansuh <nick02190219@gmail.com>
2022-03-09 19:03:16 +0900

    feat: 페이지 route 설정

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,4 +1,27 @@
-import { Colors, HomePage } from '../pages/index.js';
+import {
+  Calculator,
+  Colors,
+  Counter,
+  DigitalClock,
+  Flashcards,
+  GroceryList,
+  HexColorsGradient,
+  HomePage,
+  ImageCarousel,
+  Math,
+  QuizApp,
+  RandomQuoteGenerator,
+  SquareCards,
+  StickyNotes,
+  TheMessage,
+  Timer,
+  TipCalculator,
+  TodoList,
+  Typewriter,
+  UnsplashAPI,
+  WeatherApplication,
+  WikipediaAPI,
+} from '../pages/index.js';
 import { initRouter } from './routeHook.js';
 
 export default function Route({ targetEl }) {
@@ -13,7 +36,67 @@ export default function Route({ targetEl }) {
         break;
       case '/Colors':
         new Colors({ targetEl });
-
+        break;
+      case '/Hex-colors-gradient':
+        new HexColorsGradient({ targetEl });
+        break;
+      case '/Random-quote-generator':
+        new RandomQuoteGenerator({ targetEl });
+        break;
+      case '/The-message':
+        new TheMessage({ targetEl });
+        break;
+      case '/Counter':
+        new Counter({ targetEl });
+        break;
+      case '/Image-carousel':
+        new ImageCarousel({ targetEl });
+        break;
+      case '/Digital-clock':
+        new DigitalClock({ targetEl });
+        break;
+      case '/Calculator':
+        new Calculator({ targetEl });
+        break;
+      case '/Grocery-list':
+        new GroceryList({ targetEl });
+        break;
+      case '/Tip-calculator':
+        new TipCalculator({ targetEl });
+        break;
+      case '/Flashcards':
+        new Flashcards({ targetEl });
+        break;
+      case '/Todo-list':
+        new TodoList({ targetEl });
+        break;
+      case '/Sticky-notes':
+        new StickyNotes({ targetEl });
+        break;
+      case '/Timer':
+        new Timer({ targetEl });
+        break;
+      case '/Math':
+        new Math({ targetEl });
+        break;
+      case '/Unsplash-API':
+        new UnsplashAPI({ targetEl });
+        break;
+      case '/Typewriter':
+        new Typewriter({ targetEl });
+        break;
+      case '/Square-cards':
+        new SquareCards({ targetEl });
+        break;
+      case '/Weather-application':
+        new WeatherApplication({ targetEl });
+        break;
+      case '/Wikipedia-API':
+        new WikipediaAPI({ targetEl });
+        break;
+      case '/Quiz-app':
+        new QuizApp({ targetEl });
+        break;
       default:
         break;
     }
@@ -22,28 +105,70 @@ export default function Route({ targetEl }) {
   this.route();
 
   initRouter(() => this.route());
+
+  window.addEventListener('popstate', () => {
+    this.route();
+  });
 }
 
 /* 
 /01-Colors',
 /02-Hex-colors-gradient',
-/03-Random-quote-generator',
-/04-The-message',
-/05-Counter',
-/06-Image-carousel',
-/07-Digital-clock',
-/08-Calculator',
-/09-Grocery-list',
-/10-Tip-calculator',
-/11-Flashcards',
-/12-Todo-list',
-/13-Sticky-notes',
-/14-Timer',
-/15-Math',
-/16-Unsplash-API',
-/17-Typewriter',
-/18-Square-cards',
-/19-Weather-application',
-/20-Wikipedia-API',
-/22-Quiz-app',
+case '/Random-quote-generator':
+  new ({targetEl})
+  break;
+case '/The-message':
+  new ({targetEl})
+  break;
+case '/Counter':
+  new ({targetEl})
+  break;
+case '/Image-carousel':
+  new ({targetEl})
+  break;
+case '/Digital-clock':
+  new ({targetEl})
+  break;
+case '/Calculator':
+  new ({targetEl})
+  break;
+case '/Grocery-list':
+  new ({targetEl})
+  break;
+case '/Tip-calculator':
+  new ({targetEl})
+  break;
+case '/Flashcards':
+  new ({targetEl})
+  break;
+case '/Todo-list':
+  new ({targetEl})
+  break;
+case '/Sticky-notes':
+  new ({targetEl})
+  break;
+case '/Timer':
+  new ({targetEl})
+  break;
+case '/Math':
+  new ({targetEl})
+  break;
+case '/Unsplash-API':
+  new ({targetEl})
+  break;
+case '/Typewriter':
+  new ({targetEl})
+  break;
+case '/Square-cards':
+  new ({targetEl})
+  break;
+case '/Weather-application':
+  new ({targetEl})
+  break;
+case '/Wikipedia-API':
+  new ({targetEl})
+  break;
+case '/Quiz-app':
+  new ({targetEl})
+  break;
 */
--- src/router/routeHook.js
+++ src/router/routeHook.js
@@ -2,8 +2,6 @@ const ROUTE_CHANGE_EVENT_NAME = 'route-change';
 
 export const initRouter = (onRoute) => {
   window.addEventListener(ROUTE_CHANGE_EVENT_NAME, (e) => {
-    console.log(e);
-    console.log(e.detail);
     const { nextUrl } = e.detail;
 
     if (nextUrl) {
